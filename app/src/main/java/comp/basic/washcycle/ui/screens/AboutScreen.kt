c68953787d9f93cfa666054fb97a23e7d0ed2b9a
powercomp750 <developeraryan.750@gmail.com>
2026-04-01 16:28:44 +0530

    Add AboutScreen composable for application information display

---
+++ app/src/main/java/comp/basic/washcycle/ui/screens/AboutScreen.kt
@@ -0,0 +1,9 @@
+package comp.basic.washcycle.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AboutScreen() {
+    Text("This is the About Screen")
+}
