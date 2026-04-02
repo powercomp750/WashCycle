d7186d852d55d68f305d786a3b43c7644081a463
powercomp750 <developeraryan.750@gmail.com>
2026-04-02 22:57:09 +0530

    Add LaundrySessionDao interface for managing laundry session data

---
+++ app/src/main/java/comp/basic/washcycle/data/LaundrySessionDAO.kt
@@ -0,0 +1,16 @@
+package comp.basic.washcycle.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface LaundrySessionDao {
+
+}
+
+
