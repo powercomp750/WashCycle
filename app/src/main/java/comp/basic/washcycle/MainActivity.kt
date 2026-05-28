5e656ee634f9cc40070d46fa23600ca560e813fe
powercomp750 <developeraryan.750@gmail.com>
2026-05-28 23:13:19 +0530

    Set up MainActivity with database, repository, and ViewModel integration

--- app/src/main/java/comp/basic/washcycle/MainActivity.kt
+++ app/src/main/java/comp/basic/washcycle/MainActivity.kt
@@ -5,7 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
+import androidx.lifecycle.viewmodel.compose.viewModel
+import comp.basic.washcycle.data.LaundryDatabase
+import comp.basic.washcycle.repository.LaundrySessionRepository
+import comp.basic.washcycle.ui.screens.AppNavHost
 import comp.basic.washcycle.ui.theme.WashCycleTheme
+import comp.basic.washcycle.viewmodel.LaundrySessionViewModel
 
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +20,19 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             WashCycleTheme {
-                Text("Hello World")
+                // Database
+                val database = LaundryDatabase.getDatabase(applicationContext)
+
+                // Repository
+                //val repository = LaundrySessionRepository()
+
+                // ViewModel
+                val viewModel: LaundrySessionViewModel = viewModel()
+
+                // Navigation
+                AppNavHost(
+                    viewModel = viewModel
+                )
             }
         }
     }
