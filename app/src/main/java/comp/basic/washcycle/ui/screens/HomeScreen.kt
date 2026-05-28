acbf67006de5651b53db565e037b86916d7d5ca6
powercomp750 <developeraryan.750@gmail.com>
2026-05-28 23:12:35 +0530

    Update AddLaundrySessionScreen to accept ViewModel and Settings, define predefined categories

--- app/src/main/java/comp/basic/washcycle/ui/screens/HomeScreen.kt
+++ app/src/main/java/comp/basic/washcycle/ui/screens/HomeScreen.kt
@@ -1,10 +1,17 @@
 package comp.basic.washcycle.ui.screens
 
 import androidx.compose.runtime.Composable
+import comp.basic.washcycle.data.Settings
+import comp.basic.washcycle.viewmodel.LaundrySessionViewModel
 
 
 @Composable
-fun AddLaundrySessionScreen() {
+fun AddLaundrySessionScreen(viewModel: LaundrySessionViewModel, settings: Settings) {
+
+    //The clothes I mostly handle
+    val predefinedCategories = listOf(
+        "Shirts","T-Shirts","Jeans","Trousers/Pants","Vests","Underwear","Napkins","Socks","Bedsheets","Others"
+    )
 
 }
 
